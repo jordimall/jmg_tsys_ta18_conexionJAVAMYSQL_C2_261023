53d3c27c7679f3e169b6bedb9b9d1805464c1667
jordimall <jordi.mallafre@hotmail.com>
2023-10-26 15:39:50 +0200

    Finalización ejercicio 3

---
+++ TA18_conexionJavaMysql_jordiMallafre/src/ejercicio03/Ejercicio03App.java
@@ -0,0 +1,13 @@
+package ejercicio03;
+
+import utils.UtilEjercicio03;
+
+public class Ejercicio03App {
+
+	public static void main(String[] args) {
+		UtilEjercicio03 util = new UtilEjercicio03();
+		util.iniciarAplicación();
+
+	}
+
+}
